a3285862ec3b0e21423d06a8047b666810756277
Eduardo Riki <eduardomatushita1@gmail.com>
2024-08-13 01:05:25 -0300

    chore: Mudanças mínimas

--- src/index.ts
+++ src/index.ts
@@ -3,7 +3,7 @@ import { server } from "./server";
 
 const startServer = () => {
   server.listen(process.env.PORT || 3333, () =>
-    console.log(`Rodando na porta ${process.env.PORT || 3333}`)
+    console.log(`Rodando na porta ${process.env.PORT || 3333}`),
   );
 };
 
