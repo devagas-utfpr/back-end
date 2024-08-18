9b79cb11d65735a9535d18b4a26483d33c596668
Joao Pedro <joaopedrobautista@alunos.utfpr.edu.br>
2024-08-18 10:20:28 -0300

    Adicionado rotas para requisito

--- src/main/routes/routes.ts
+++ src/main/routes/routes.ts
@@ -2,8 +2,7 @@ import { Router } from "express";
 import { EmpresaController } from "../../presentation/controllers/empresa";
 import { UsuarioController } from "../../presentation/controllers/usuario";
 import { CargoController } from "../../presentation/controllers/cargo";
-
-
+import { RequisitoController } from "../../presentation/controllers/requisito"
 const router = Router();
 
 router.post(
@@ -96,4 +95,33 @@ router.delete(
   CargoController.deleteByUUID
 );
 
+router.post(
+  "/requisitos",
+  RequisitoController.createValidation,
+  RequisitoController.create
+);
+
+router.get(
+  "/requisitos",
+  RequisitoController.getAllValidation,
+  RequisitoController.getAll
+);
+
+router.get(
+  "/requisitos/:uuid",
+  RequisitoController.getByUUIDValidation,
+  RequisitoController.getByUUID
+);
+
+router.put(
+  "/requisitos/:uuid",
+  RequisitoController.updateByUUIDValidation,
+  RequisitoController.updateByUUID
+);
+
+router.delete(
+  "/requisitos/:uuid",
+  RequisitoController.deleteByUUIDValidation,
+  RequisitoController.deleteByUUID
+);
 export default router;
