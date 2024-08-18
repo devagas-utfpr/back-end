55c13d6008421b47295ecbd3306d5d7eda695da4
Joao Pedro <joaopedrobautista@alunos.utfpr.edu.br>
2024-08-17 22:03:59 -0300

    adicionado rotas de cargo e empresa

--- src/main/routes/routes.ts
+++ src/main/routes/routes.ts
@@ -1,13 +1,99 @@
 import { Router } from "express";
-import { PrismaEmpresaRepository } from "../../infrastructure/services/EmpresaServices";
-import { EmpresaController } from "../../presentation/controllers/EmpresaController";
+import { EmpresaController } from "../../presentation/controllers/empresa";
+import { UsuarioController } from "../../presentation/controllers/usuario";
+import { CargoController } from "../../presentation/controllers/cargo";
+
 
 const router = Router();
-const empresaRepository = new PrismaEmpresaRepository();
-const empresaController = new EmpresaController(empresaRepository);
 
-router.get("/empresas", (req, res) =>
-  empresaController.getAll(res)
+router.post(
+  "/empresas",
+  EmpresaController.createValidation,
+  EmpresaController.create
+);
+
+router.get(
+  "/empresas",
+  EmpresaController.getAllValidation,
+  EmpresaController.getAll
+);
+
+router.get(
+  "/empresas/:uuid",
+  EmpresaController.getByUUIDValidation,
+  EmpresaController.getByUUID
+);
+
+router.put(
+  "/empresas/:uuid",
+  EmpresaController.updateByUUIDValidation,
+  EmpresaController.updateByUUID
+);
+
+router.delete(
+  "/empresas/:uuid",
+  EmpresaController.deleteByUUIDValidation,
+  EmpresaController.deleteByUUID
+);
+
+router.post(
+  "/usuarios",
+  UsuarioController.createValidation,
+  UsuarioController.create
+);
+
+router.get(
+  "/usuarios",
+  UsuarioController.getAllValidation,
+  UsuarioController.getAll
+);
+
+router.get(
+  "/usuarios/:uuid",
+  UsuarioController.getByUUIDValidation,
+  UsuarioController.getByUUID
+);
+
+router.put(
+  "/usuarios/:uuid",
+  UsuarioController.updateByUUIDValidation,
+  UsuarioController.updateByUUID
+);
+
+router.delete(
+  "/usuarios/:uuid",
+  UsuarioController.deleteByUUIDValidation,
+  UsuarioController.deleteByUUID
+);
+
+router.post(
+  "/cargos",
+  CargoController.createValidation,
+  CargoController.create
+);
+
+router.get(
+  "/cargos",
+  CargoController.getAllValidation,
+  CargoController.getAll
+);
+
+router.get(
+  "/cargos/:uuid",
+  CargoController.getByUUIDValidation,
+  CargoController.getByUUID
+);
+
+router.put(
+  "/cargos/:uuid",
+  CargoController.updateByUUIDValidation,
+  CargoController.updateByUUID
+);
+
+router.delete(
+  "/cargos/:uuid",
+  CargoController.deleteByUUIDValidation,
+  CargoController.deleteByUUID
 );
 
 export default router;
