3db2a728cc2acc2977661b01c7fad56242ae8b47
Eduardo Riki <eduardomatushita1@gmail.com>
2024-09-04 00:47:29 -0300

    feat: Adicionar login para retornar token

---
+++ src/presentation/controllers/auth/SignIn.ts
@@ -0,0 +1,59 @@
+import { Request, Response } from "express";
+import { StatusCodes } from "http-status-codes";
+import * as Yup from "yup";
+
+import { validation } from "../../middlewares/Validation";
+import { Usuario } from "../../../domain/entities/Usuario";
+import { UsuarioService } from "../../../infrastructure/services";
+import { PasswordCrypto } from "../../../infrastructure/services/auth/PasswordCrypto";
+import { JWTService } from "../../../infrastructure/services/auth/JwtService";
+
+interface IBodyProps
+  extends Omit<
+    Usuario,
+    "uuid" | "nome" | "cpf" | "dataNascimento" | "createdAt" | "updatedAt"
+  > {}
+
+export const signInValidation = validation((getSchema) => ({
+  body: getSchema<IBodyProps>(
+    Yup.object().shape({
+      email: Yup.string().required().email(),
+      senha: Yup.string().required().min(6),
+    })
+  ),
+}));
+
+export const signIn = async (req: Request<{}, {}, Usuario>, res: Response) => {
+  const { email, senha } = req.body;
+
+  const usuario = await UsuarioService.getByEmail(email);
+  if (usuario instanceof Error) {
+    return res.status(StatusCodes.UNAUTHORIZED).json({
+      errors: {
+        default: "Email e/ou senha são inválidos",
+      },
+    });
+  }
+
+  const isPasswordValid = await PasswordCrypto.verifyPassword(
+    senha,
+    usuario.senha
+  );
+  if (!isPasswordValid) {
+    return res.status(StatusCodes.UNAUTHORIZED).json({
+      errors: {
+        default: "Email e/ou senha são inválidos",
+      },
+    });
+  }
+
+  const accessToken = JWTService.sign({ uuid: usuario.uuid });
+  if (accessToken === "JWT_SECRET_NOT_FOUND") {
+    return res.status(StatusCodes.INTERNAL_SERVER_ERROR).json({
+      errors: {
+        default: "Erro ao gerar token de acesso.",
+      },
+    });
+  }
+  return res.status(StatusCodes.OK).json({ accessToken });
+};
